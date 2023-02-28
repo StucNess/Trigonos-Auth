d2fb8737ff05a5d50e4b83e825ac93af1aba3d73
Elvis <ejoocontactos@gmail.com>
2023-02-28 10:25:54 -0300

    registrar usuario

--- src/app/auth/services/jwtService/jwtServiceConfig.js
+++ src/app/auth/services/jwtService/jwtServiceConfig.js
@@ -1,9 +1,9 @@
 const jwtServiceConfig = {
   signIn: "http://164.77.112.10:99/api/Usuarios/Login",
-  signUp: "api/auth/sign-up",
+  signUp: "http://164.77.112.10:99/api/Usuarios/Registrar",
+  addProyects: "http://164.77.112.10:99/api/Usuarios/AsignarProyecto",
   accessToken: "http://164.77.112.10:99/api/Usuarios",
   updateUser: "api/auth/user/update",
 };
 
 export default jwtServiceConfig;
-
