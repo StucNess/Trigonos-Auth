f337dcc82192c69e5bd312a83e62cb33e2411351
Andres <edgar.proboste@gmail.com>
2023-05-30 22:36:48 -0400

    avp tipocliente

--- src/app/main/comercial/Participantesv2/tabs/FiltrosParticipant.js
+++ src/app/main/comercial/Participantesv2/tabs/FiltrosParticipant.js
@@ -1,54 +1,79 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
+
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
-import SearchIcon from "@mui/icons-material/Search";
+import Stack from "@mui/material/Stack";
+import LinearProgress from "@mui/material/LinearProgress";
+
 import { useEffect, useState } from "react";
 import Autocomplete from "@mui/material/Autocomplete";
-import { CallApi } from "../store/CallApi";
 import AdviceModule from "../../AdviceModule";
+import { useGetProyectoAllMutation, useGetProyectoByIdMutation } from "app/store/participantesApi/participantesApi";
 
 let participants;
 export default function FiltrosParticipant(props) {
-  const [render, setRender] = useState(false);
-  const [nameParticipants, setNameParticipants] = useState([]);
+  const [getParticipant, data] = useGetProyectoAllMutation();
+  const [getProyect, dataProyect] = useGetProyectoByIdMutation();
+  const [carga, setCarga] = useState(true);
+  const [nameParticipants, setNameParticipants] = useState([{
+    name: "Vacio",
+    rutCompleto: "Vacio",
+    id: 1,
+  },]);
   const [dataParticipant, setDataParticipant] = useState([
     {
-      name: "EcoMetales Limited, Agencia en Chile",
-      rutCompleto: "59087530-9",
+      name: "Vacio",
+      rutCompleto: "Vacio",
       id: 1,
     },
   ]);
-
+  
+  // useEffect(() => {
+  //   props.isLoading(data.isLoading);
+  // }, [data.isLoading]);
+  //corregir error de datos indefinidos
+  useEffect(() => {
+    if(!data.isLoading){
+      setCarga(false);
+    }
+    console.log(data.isLoading);
+  }, [data.isLoading]);
   useEffect(() => {
     props.sendParticipants(dataParticipant);
+    
   }, [dataParticipant]);
   useEffect(() => {
     if (props.change) {
-      // console.log(props.change);
+    
       `ID DEL REFRESH: ${props.idParticipant}`;
       callAsyncApi(props.idParticipant);
     }
   }, [props.change]);
   let callAsyncApi = (refreshValue) => {
-    (async () => {
-      participants = await CallApi(1, 1000, 1);
+    getParticipant({PageIndex:1,PageSize:1000}).then((response)=>{
+      let participants = response.data.data;
       setNameParticipants(participants);
-      console.log(participants)
       setDataParticipant(participants[parseInt(refreshValue - 1)]);
       props.sendParticipants(participants[parseInt(refreshValue - 1)]);
-      // props.sendParticipants(participants[parseInt(refreshValue-1)]);
-    })();
+    }).catch((error)=>{
+      
+    });
+ 
   };
   useEffect(() => {
-    (async () => {
-      participants = await CallApi(1, 1000, 1);
-      setNameParticipants(participants);
-      props.sendParticipants(participants[0]);
-    })();
+    getParticipant({PageIndex:1,PageSize:1000}).then((response)=>{
+      props.sendParticipants(response.data.data[0]);
+      setNameParticipants(response.data.data);
+      setDataParticipant(response.data.data[0])
+      
+      
+    }).catch((error)=>{
+      
+    });
+    
   }, []);
-  // (dataParticipant.name);
+  
   return (
     <Box>
       <Box className="flex flex-col w-full mb-[20px] ">
@@ -67,50 +92,53 @@ export default function FiltrosParticipant(props) {
         </Typography>
         <span>Introducir términos de búsqueda</span>
       </Box>
-      <Box className="flex  w-full   mdmax:flex-wrap justify-evenly   lg:justify-start">
-        {/* <TextField
-          className="zerorange:w-[300px]  lg:w-[400px] w-[200px]  mdmax:m-[20px]"
-          label="Ingrese nombre comercial"
-          type="text"
-          variant="filled"
-        /> */}
-        <Autocomplete
-          className="m-[10px] w-[300px] md:w-[500px]"
-          disablePortal
-          options={nameParticipants}
-          value={dataParticipant}
-          onChange={(event, newValue) =>
-            newValue != undefined && setDataParticipant(newValue)
-          }
-          getOptionLabel={(option) =>
-            option.business_Name || dataParticipant[0].name
-          }
-          // isOptionEqualToValue={(option, value) =>
-          //   dataParticipant.name == value || ""
-          // }
-          id="combo-box-demo"
-          renderInput={(params) => (
-            <TextField {...params} label="Razón Social" />
-          )}
-        />
-        <Autocomplete
-          className="m-[10px] w-[300px] md:w-[500px]"
-          disablePortal
-          onChange={(event, newValue) =>
-            newValue != undefined && setDataParticipant(newValue)
-          }
-          // isOptionEqualToValue={(option, value) =>
-          //   dataParticipant.name == value || ""
-          // }
-          getOptionLabel={(option) =>
-            option.rutCompleto || dataParticipant[0].rutCompleto
-          }
-          id="combo-box-demo"
-          options={nameParticipants}
-          value={dataParticipant}
-          renderInput={(params) => <TextField {...params} label="Rut" />}
-        />
-      </Box>
+      {carga?
+              <div className="flex items-center">
+                <Stack sx={{ width: "100%", color: "grey.500" }} spacing={2}>
+                  {/* <p>Chupa Chupa .....</p> */}
+                  <LinearProgress color="primary" />
+                </Stack>
+              </div>:
+              <Box className="flex  w-full   mdmax:flex-wrap justify-evenly   lg:justify-start">
+              <Autocomplete
+                className="m-[10px] w-[300px] md:w-[500px]"
+                disablePortal
+                options={nameParticipants}
+                value={dataParticipant}
+                onChange={(event, newValue) =>
+                  newValue != undefined && setDataParticipant(newValue)
+                }
+                getOptionLabel={(option) =>
+                  option.business_Name || dataParticipant[0].name
+                }
+                // isOptionEqualToValue={(option, value) =>
+                //   dataParticipant.name == value || ""
+                // }
+                id="combo-box-demo"
+                renderInput={(params) => (
+                  <TextField {...params} label="Razón Social" />
+                )}
+              />
+              <Autocomplete
+                className="m-[10px] w-[300px] md:w-[500px]"
+                disablePortal
+                onChange={(event, newValue) =>
+                  newValue != undefined && setDataParticipant(newValue)
+                }
+                // isOptionEqualToValue={(option, value) =>
+                //   dataParticipant.name == value || ""
+                // }
+                getOptionLabel={(option) =>
+                  option.rutCompleto || dataParticipant[0].rutCompleto
+              
+                }
+                id="combo-box-demo"
+                options={nameParticipants}
+                value={dataParticipant}
+                renderInput={(params) => <TextField {...params} label="Rut" />}
+              />
+            </Box>}
+      
     </Box>
   );
 }
