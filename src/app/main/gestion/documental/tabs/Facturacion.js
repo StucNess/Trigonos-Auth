45e4afb112bbd79f4b503a3ba98c12a1e7819f31
Elvis Olmedo <ejoocontactos@gmail.com>
2023-06-07 16:08:24 -0400

    s

--- src/app/main/gestion/documental/tabs/Facturacion.js
+++ src/app/main/gestion/documental/tabs/Facturacion.js
@@ -92,7 +92,7 @@ export default function Facturacion(props) {
         dataPrueba.push(row);
       });
       let url;
-      url = `http://localhost:5205/api/Instrucciones/acturalizarFacturacion?id=${props.idParticipant}`;
+      url = `https://trigonosapi.azurewebsites.net/api/Instrucciones/acturalizarFacturacion?id=${props.idParticipant}`;
       axios
         .post(url, dataPrueba)
         .then(function (response) {
@@ -107,7 +107,7 @@ export default function Facturacion(props) {
             type: type,
             description: description,
           };
-          let url = `http://localhost:5205/api/Instrucciones/Agregar`;
+          let url = `https://trigonosapi.azurewebsites.net/api/Instrucciones/Agregar`;
           axios
             .post(url, json)
             .then(function (response) {
@@ -130,7 +130,8 @@ export default function Facturacion(props) {
             type: type,
             description: error.response.data.message,
           };
-          let url = "http://localhost:5205/api/Instrucciones/Agregar";
+          let url =
+            "https://trigonosapi.azurewebsites.net/api/Instrucciones/Agregar";
           axios
             .post(url, json)
             .then(function (response) {
